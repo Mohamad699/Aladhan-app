ea5e15bae8799c301808dfe53b9d84221eb0d4c5
Mohamad699 <slman88777@gmail.com>
2026-02-12 23:38:46 +0300

    Update prayer.service.ts

--- src/app/services/prayer.service.ts
+++ src/app/services/prayer.service.ts
@@ -1,4 +1,3 @@
-
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { switchMap, map, throwError } from 'rxjs';
@@ -10,38 +9,48 @@ export class PrayerService {
 
   getPrayerTimes(city: string) {
 
-    // 🔹 Geocoding (يدعم العربية)
     const geoParams = new HttpParams()
-      .set('name', city)
+      .set('name', city)       
       .set('count', '1')
-      .set('language', 'ar');
-
+      .set('language', 'ar'); 
+      
     return this.http
       .get<any>('https://geocoding-api.open-meteo.com/v1/search', { params: geoParams })
       .pipe(
         switchMap(geo => {
+
+          console.log('GEO RESPONSE:', geo);
+
           if (!geo.results || geo.results.length === 0) {
             return throwError(() => new Error('CITY_NOT_FOUND'));
           }
 
           const { latitude, longitude, name, country } = geo.results[0];
 
-          // 🔹 Prayer API
+          const today = new Date().toISOString().split('T')[0];
+
           const prayerParams = new HttpParams()
-            .set('latitude', latitude)
-            .set('longitude', longitude)
-            .set('method', '2');
+            .set('latitude', latitude.toString())
+            .set('longitude', longitude.toString());
 
           return this.http
-            .get<any>('https://api.aladhan.com/v1/timings', { params: prayerParams })
+            .get<any>(`https://api.aladhan.com/v1/timings/${today}`, { params: prayerParams })
             .pipe(
-              map(res => ({
-                city: name,
-                country,
-                latitude,
-                longitude,
-                timings: res.data.timings
-              }))
+              map(res => {
+                const t = res.data.timings;
+
+                return {
+                  city: name,       
+                  country,          
+                  timings: {
+                    Fajr: t.Fajr.split(' ')[0],
+                    Dhuhr: t.Dhuhr.split(' ')[0],
+                    Asr: t.Asr.split(' ')[0],
+                    Maghrib: t.Maghrib.split(' ')[0],
+                    Isha: t.Isha.split(' ')[0]
+                  }
+                };
+              })
             );
         })
       );
