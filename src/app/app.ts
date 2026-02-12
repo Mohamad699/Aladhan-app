b9c8c3d6d43d2f20686ad243dbf3a468fe8d2d37
Mohamad699 <slman88777@gmail.com>
2026-02-12 23:40:06 +0300

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,8 @@
-
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { PrayerService } from './services/prayer.service';
-import { ChangeDetectorRef } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,39 +10,38 @@ import { ChangeDetectorRef } from '@angular/core';
   templateUrl: './app.html'
 })
 export class App {
-
   city = '';
   data: any = null;
   loading = false;
   error = '';
-constructor(
-  private prayerService: PrayerService,
-  private cdr: ChangeDetectorRef
-) {}
+
+  constructor(private prayerService: PrayerService) {
+   
+    const savedCity = localStorage.getItem('city');
+    if (savedCity) {
+      this.city = savedCity;
+    }
+  }
 search() {
   const city = this.city.trim();
   if (!city) return;
 
   this.loading = true;
   this.error = '';
-  this.data = null;
 
   this.prayerService.getPrayerTimes(city).subscribe({
     next: res => {
-      console.log('Prayer Times Response:', res);
-      console.log('قبل', this.loading);
       this.data = res;
-      this.loading = false; // ✅ سيُحدّث الواجهة فورًا
-       this.cdr.detectChanges();
-      console.log('بعد', this.loading);
+      localStorage.setItem('prayerData', JSON.stringify(res));
+      localStorage.setItem('city', city);
+      this.loading = false;
     },
     error: () => {
       this.error = 'لم يتم العثور على المدينة';
       this.loading = false;
-       this.cdr.detectChanges();
     }
   });
 }
-
-
 }
+
+
